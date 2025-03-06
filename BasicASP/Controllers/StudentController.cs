f7373e4ba630a654d2934fc9d1c3f0dbdbce41a4
siriwan limsakul <siriwan.limsakul@sato-global.com>
2025-03-06 15:19:52 +0700

    2025-03-06

---
+++ BasicASP/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using BasicASP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicASP.Controllers
+{
+	public class StudentController : Controller
+	{
+		public IActionResult Index()
+		{
+			Student student = new Student();	
+			student.Id = 1;
+			student.Name = "Sirisopha";
+			student.Score = 99;
+
+			var st = new Student();
+			st.Id = 2;
+			st.Name = "Siriwan";
+			st.Score = 88;
+
+			Student std = new();
+			std.Id = 3;
+			std.Name = "Kosol";
+			std.Score = 0;
+
+			List<Student> students = new List<Student>();
+			students.Add(student);
+			students.Add(std);
+			students.Add(st);
+
+			return View(students);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		public IActionResult Update(int id)
+		{
+			return Content($"Detail {id}");
+		}
+
+		public IActionResult Delete(int id)
+		{
+			return Content($"Detail {id}");
+		}
+	}
+}
---
+++ BasicASP/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicASP.Models
+{
+	public class Student
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string? Name { get; set; }
+
+
+		public int Score { get; set; }
+	}
+}
